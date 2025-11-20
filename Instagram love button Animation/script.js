ed810216d762b9deb00a0a7029d0be2731136a6d
Shreya <sreya-builds@users.noreply.github.com>
2025-11-20 19:37:05 +0530

    new timing

--- Instagram love button Animation/script.js
+++ Instagram love button Animation/script.js
@@ -9,6 +9,6 @@ cards.forEach(function(card){
     setTimeout(()=>{
         love.style.opacity=0
         love.style.transform = 'translate(-50%,-50%) scale(1) rotate(60deg)'
-    },6000);
+    },2000);
 });
 });
